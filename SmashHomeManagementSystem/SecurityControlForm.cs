42965f0f653ce1d9bd5dcb1ed00f69097036fcbc
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2025-02-26 23:16:23 +0700

    Add door control button and TCP client connection
    - Modified `InitializeComponent` in `SecurityControlForm.Designer.cs` to add `button1` with properties and `Click` event handler.
    - Updated `SecurityControlForm` in `SecurityControlForm.cs` to include `_doorClient` field and asynchronous connection to TCP server on `localhost:2374` with error logging.
    - Added `button1_Click` event handler to send "Open" message to TCP server and read response asynchronously with error logging.
    - Added `Message` enumeration to define `Open` and `Close` messages.
    - Updated `SmashHomeManagementSystem.csproj` to include new embedded resource `SecurityControlForm.resx`.
    - Added `SecurityControlForm.resx` for resource management.

--- SmashHomeManagementSystem/SecurityControlForm.cs
+++ SmashHomeManagementSystem/SecurityControlForm.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +14,24 @@ namespace SmashHomeManagementSystem
 {
     public partial class SecurityControlForm : Form
     {
+        private TcpClient _doorClient = null;
         public SecurityControlForm()
         {
             InitializeComponent();
+
         }
 
-        private void SecurityControlForm_Load(object sender, EventArgs e)
+        private async void SecurityControlForm_Load(object sender, EventArgs e)
         {
-            // Initialize security control UI
+            try
+            {
+                _doorClient = new TcpClient();
+                await _doorClient.ConnectAsync("localhost", 2374).ConfigureAwait(false);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
         private void btnArm_Click(object sender, EventArgs e)
@@ -31,5 +43,29 @@ namespace SmashHomeManagementSystem
         {
             // Logic to disarm the security system
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (_doorClient is null) { return; }
+
+            byte[] data = Encoding.UTF8.GetBytes(Message.Open.ToString());
+            try
+            {
+                await _doorClient.GetStream().WriteAsync(data, 0, data.Length).ConfigureAwait(false);
+
+                await _doorClient.GetStream().ReadAsync(data, 0, data.Length).ConfigureAwait(false);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    enum Message
+    {
+        Open,
+        Close,
     }
 }
